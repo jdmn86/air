bd600b3f2b449b6632bca2e70a31348cb6c26699
Ricardo Pinto <2140679@my.ipleiria.pt>
2017-11-06 18:08:39 +0000

    Adicionei classe de listener para os sensores, fiz bindings das textviews para variaveis, criei metodos para alterar os valores das textviews

---
+++ app/src/main/java/pt/ipleiria/dei/iair/model/IAirSensorListener.java
@@ -0,0 +1,32 @@
+package pt.ipleiria.dei.iair.model;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+/**
+ * Created by ricar on 06/11/2017.
+ */
+
+public class IAirSensorListener implements SensorEventListener {
+
+    private float[] eventValues;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        eventValues = event.values.clone();
+        int sensorType = event.sensor.getType();
+        if (sensorType == Sensor.TYPE_AMBIENT_TEMPERATURE){
+            IAirManager.INSTANCE.changeTemperatureValue(eventValues);
+        } else if (sensorType == Sensor.TYPE_RELATIVE_HUMIDITY){
+            IAirManager.INSTANCE.changeHumidityValue(eventValues);
+        } else if (sensorType == Sensor.TYPE_PRESSURE){
+            IAirManager.INSTANCE.changePressureValue(eventValues);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
--- app/src/main/java/pt/ipleiria/dei/iair/view/MySensorsActivity.java
+++ app/src/main/java/pt/ipleiria/dei/iair/view/MySensorsActivity.java
@@ -1,21 +1,74 @@
 package pt.ipleiria.dei.iair.view;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import pt.ipleiria.dei.iair.R;
+import pt.ipleiria.dei.iair.model.IAirManager;
 
 public class MySensorsActivity extends AppCompatActivity {
+
+    private TextView temperatureSensorValue,humiditySensorValue,pressureSensorValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_sensors);
+        IAirManager.INSTANCE.setMySensorsActivity(this);
+        bindTextViews();
+        setSensors();
+
+
     }
+
+    private void setSensors() {
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        IAirManager.INSTANCE.setSensorManager(sensorManager);
+
+        try {
+            Sensor temperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+            IAirManager.INSTANCE.setSensor(temperatureSensor);
+            temperatureSensorValue.setText("0.0");
+        }catch (Exception e){
+            temperatureSensorValue.setText("N/A");
+            Toast.makeText(this, R.string.toast_no_temperature_sensor_available,Toast.LENGTH_SHORT).show();
+        }
+        try {
+            Sensor pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+            IAirManager.INSTANCE.setSensor(pressureSensor);
+            pressureSensorValue.setText("0.0");
+        }catch (Exception e){
+            pressureSensorValue.setText("N/A");
+            Toast.makeText(this, R.string.toast_no_pressure_sensor_available,Toast.LENGTH_SHORT).show();
+        }
+        try {
+            Sensor humiditySensor = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+            IAirManager.INSTANCE.setSensor(humiditySensor);
+            humiditySensorValue.setText("0.0");
+        }catch (Exception e){
+            humiditySensorValue.setText("N/A");
+            Toast.makeText(this, R.string.toast_no_humidity_sensor_available,Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void bindTextViews() {
+        temperatureSensorValue = (TextView) findViewById(R.id.textViewTemperatureSensorValue);
+        pressureSensorValue = (TextView) findViewById(R.id.textViewPressureSensorValue);
+        humiditySensorValue = (TextView) findViewById(R.id.textViewHumiditySensorValue);
+    }
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
@@ -53,4 +106,16 @@ public class MySensorsActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void setTemperatureValue(float eventValue) {
+        temperatureSensorValue.setText(String.valueOf(eventValue) + " ºC");
+    }
+
+    public void setHumidityValue(float eventValue) {
+        humiditySensorValue.setText(String.valueOf(eventValue) + " %");
+    }
+
+    public void setPressureValue(float eventValue) {
+        pressureSensorValue.setText(String.valueOf(eventValue) + " hpa");
+    }
+
 }
