8c3cd599aa1c10d0bc5558ccc47aa3d6e92b3baa
Teotonio95 <davidteotonio07@hotmail.com>
2017-11-16 01:34:45 +0000

    Layout concluido e Dialog criada

--- app/src/main/java/pt/ipleiria/dei/iair/view/DashboardActivity.java
+++ app/src/main/java/pt/ipleiria/dei/iair/view/DashboardActivity.java
@@ -1,23 +1,165 @@
 package pt.ipleiria.dei.iair.view;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import pt.ipleiria.dei.iair.R;
+import pt.ipleiria.dei.iair.Utils.GPSUtils;
 //import pt.ipleiria.dei.iair.Utils.ThinkSpeak;
 
 public class DashboardActivity extends AppCompatActivity {
+    public static final String SHARED_PREFERENCES = "Shared";
+    SharedPreferences preferencesRead;
+    SharedPreferences.Editor preferencesWrite;
+
+    private String txtUsername = "Username: ";
+    private TextView favouriteLocationTXT;
+    private TextView temperatureFavLocationValue;
+    private TextView pressureFavLocationValue;
+    private TextView humidityFavLocationValue;
+    private String favLocation;
+    private TextView userNameTXT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        preferencesRead =getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        preferencesWrite = preferencesRead.edit();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         //ThinkSpeak.createNewChannel("Coimbra",40.200939, -8.407976,true,"Temperatura","Pressão","Humidade");
+        bindTextViews();
+        favLocation = getLocationFavourite();
+        if (favLocation == "") {
+            openDialog();
+        }
+        favouriteLocationTXT.setText(favLocation);
+        userNameTXT.setText(txtUsername + getUsername());
 
+    }
+
+    private void bindTextViews() {
+        favouriteLocationTXT = findViewById(R.id.textViewFavoriteLocation);
+        temperatureFavLocationValue = findViewById(R.id.textViewValueTemperature);
+        pressureFavLocationValue =  findViewById(R.id.textViewValuePressure);
+        humidityFavLocationValue = findViewById(R.id.textViewValueHumidity);
+        userNameTXT = findViewById(R.id.textViewUserName);
+    }
+
+
+    public void openDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setTitle("Alert");
+        alertDialogBuilder.setMessage("Do you want choose current location " + getActualLocation() + " with favourite location?");
+
+        alertDialogBuilder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                saveFavouriteLocation();
+                if (getUsername() == "") {
+                    openDialogName();
+                }
+                favouriteLocationTXT.setText(getLocationFavourite());
+                Toast.makeText(DashboardActivity.this,"Location Favourite Updated to: " + getActualLocation(),Toast.LENGTH_LONG).show();
+            }
+        });
+        alertDialogBuilder.setNegativeButton("NO",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (getUsername() == "") {
+                    openDialogName();
+                }
+                Toast.makeText(DashboardActivity.this,"Your favourite location isn't choose",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    public void openDialogName(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setTitle("Insert Name");
+        alertDialogBuilder.setMessage("Username:");
+
+        final EditText input = new EditText(DashboardActivity.this);
+        alertDialogBuilder.setView(input);
+
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                saveUsername(input.getText().toString());
+                userNameTXT.setText(txtUsername + getUsername());
+                Toast.makeText(DashboardActivity.this,"Save your Username",Toast.LENGTH_LONG).show();
+
+            }
+        });
+        alertDialogBuilder.setNegativeButton("CANCEL",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(DashboardActivity.this,"Your username isn't choose",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+
+
+    private String getActualLocation() {
+        GPSUtils gpsUtils = new GPSUtils(this);
+        Location currentLocation = gpsUtils.getLocation();
+        String actualLocation = "";
+        try {
+            actualLocation =  preferencesRead.getString("locationText", GPSUtils.getLocationDetails(this, currentLocation.getLatitude(), currentLocation.getLongitude()).getLocality());
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return actualLocation;
+    }
+
+
+    public void saveFavouriteLocation() {
+        SharedPreferences sharedPreferences = getSharedPreferences("userData", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("locationFavourite", getActualLocation());
+        editor.apply();
+    }
+
+    private void saveUsername(String username) {
+        SharedPreferences sharedPreferences = getSharedPreferences("userData", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("userName", username);
+        editor.apply();
+    }
+
+    public String getLocationFavourite () {
+        SharedPreferences sharedPreferences = getSharedPreferences("userData", Context.MODE_PRIVATE);
+        String favLocation = sharedPreferences.getString("locationFavourite", "");
+        return  favLocation;
+    }
+
+    public String getUsername() {
+        SharedPreferences sharedPreferences = getSharedPreferences("userData", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("userName", "");
+        return  username;
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
