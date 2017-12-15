fd9fc4939d26f60f0f8df4617901f97ed0931721
Ricardo Pinto <2140679@my.ipleiria.pt>
2017-12-15 17:15:04 +0000

    #TAESG12-173 teste AT4- Apresentar as localizações no spinner

--- app/src/androidTest/java/pt/ipleiria/dei/iair/US5/US5_AT4.java
+++ app/src/androidTest/java/pt/ipleiria/dei/iair/US5/US5_AT4.java
@@ -5,31 +5,22 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import pt.ipleiria.dei.iair.MasterTest;
 import pt.ipleiria.dei.iair.R;
-import pt.ipleiria.dei.iair.controller.IAirManager;
-import pt.ipleiria.dei.iair.model.CityAssociation;
 import pt.ipleiria.dei.iair.view.DashboardActivity;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -40,29 +31,21 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.hasToString;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.startsWith;
-import static org.hamcrest.core.StringContains.containsString;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class US5_AT4 extends MasterTest{
+public class US5_AT4 {
 
     @Rule
     public ActivityTestRule<DashboardActivity> mActivityTestRule = new ActivityTestRule<>(DashboardActivity.class);
 
     @Test
     public void uS5_AT4() {
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
 
-        List<String> cityNames = new ArrayList<>();
-        for (CityAssociation cityAssociation: IAirManager.INSTANCE.getAllCityAssociations()) {
-            cityNames.add(cityAssociation.getREGION_NAME());
-        }
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
 
         ViewInteraction appCompatTextView = onView(
                 allOf(withId(R.id.title), withText("Location list"),
@@ -73,15 +56,51 @@ public class US5_AT4 extends MasterTest{
                                 0),
                         isDisplayed()));
         appCompatTextView.perform(click());
-
-
-        onView(withId(R.id.spinnerLocationList)).perform(click());
-        for(int i= 0; i < cityNames.size()-1;i++){
-            Log.d("debj",cityNames.get(i)+ " :TESTE AT4 US5");
-            Log.d("debj1",cityNames.size()+ " :TESTE AT4 US5");
-            onData(hasToString(cityNames.get(i)));
-            onView(withText(R.id.spinnerLocationList)).check(matches(withText(containsString(cityNames.get(i)))));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        ViewInteraction appCompatSpinner = onView(
+                allOf(withId(R.id.spinnerLocationList),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatSpinner.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(android.R.id.text1), withText("Lisbon"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Lisbon")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(android.R.id.text1), withText("Faro"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView2.check(matches(withText("Faro")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(android.R.id.text1), withText("Batalha, Portugal"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                        0),
+                                2),
+                        isDisplayed()));
+        textView3.check(matches(withText("Batalha, Portugal")));
+
     }
 
     private static Matcher<View> childAtPosition(
